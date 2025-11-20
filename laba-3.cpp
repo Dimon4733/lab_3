4bbb71a9459290a017232d9de0cf5d07eb6a2d41
Dimon4733 <obesenkldima@gmail.com>
2025-11-20 18:14:01 +0200

    Add files via upload

---
+++ laba-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+	int x = 8
+	int y= 0
+		while (y == x) {
+			std::cin >> y
+			if (y == x) {
+				std::cout << "правильно";
+			}
+			if (y != x) {
+				std::cout<<"не правильно"
+			}
+				
+		}
+}
